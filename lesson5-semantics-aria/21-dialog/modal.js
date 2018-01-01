cf7a01720f816ae2e030bf7b0838a80444c84525
Matt Rhone <kritoke@gmail.com>
2018-01-01 10:14:01 -0600

    Fix screen reader issues with modal dialog escaping

--- lesson5-semantics-aria/21-dialog/modal.js
+++ lesson5-semantics-aria/21-dialog/modal.js
@@ -9,70 +9,73 @@ var modalToggle = document.querySelector('.modal-toggle');
 modalToggle.addEventListener('click', openModal);
 
 function openModal(e) {
-  // Save current focus
-  focusedElementBeforeModal = e.target;
+    // Save current focus
+    focusedElementBeforeModal = e.target;
 
-  // Listen for and trap the keyboard
-  modal.addEventListener('keydown', trapTabKey);
+    // Listen for and trap the keyboard
+    modal.addEventListener('keydown', trapTabKey);
 
-  // Listen for indicators to close the modal
-  modalOverlay.addEventListener('click', closeModal);
-  // Log In button
-  var closeButton = modal.querySelector('#close');
-  closeButton.addEventListener('click', closeModal);
+    // Listen for indicators to close the modal
+    modalOverlay.addEventListener('click', closeModal);
+    // Log In button
+    var closeButton = modal.querySelector('#close');
+    closeButton.addEventListener('click', closeModal);
 
-  // Find all focusable children
-  var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
-  var focusableElements = modal.querySelectorAll(focusableElementsString);
-  // Convert NodeList to Array
-  focusableElements = Array.prototype.slice.call(focusableElements);
+    // Find all focusable children
+    var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+    var focusableElements = modal.querySelectorAll(focusableElementsString);
+    // Convert NodeList to Array
+    focusableElements = Array.prototype.slice.call(focusableElements);
 
-  var firstTabStop = focusableElements[0];
-  var lastTabStop = focusableElements[focusableElements.length - 1];
+    var firstTabStop = focusableElements[0];
+    var lastTabStop = focusableElements[focusableElements.length - 1];
 
-  // Show the modal and overlay
-  modal.style.display = 'block';
-  modalOverlay.style.display = 'block';
+    // Show the modal and overlay
+    modal.style.display = 'block';
+    modalOverlay.style.display = 'block';
 
-  // Focus first child
-  firstTabStop.focus();
+    // Focus first child
+    firstTabStop.focus();
 
-  function trapTabKey(e) {
-    // Check for TAB key press
-    if (e.keyCode === 9) {
+    function trapTabKey(e) {
+        // Check for TAB key press
+        if (e.keyCode === 9) {
 
-      // SHIFT + TAB
-      if (e.shiftKey) {
-        if (document.activeElement === firstTabStop) {
-          e.preventDefault();
-          lastTabStop.focus();
+            // SHIFT + TAB
+            if (e.shiftKey) {
+                if (document.activeElement === firstTabStop) {
+                    e.preventDefault();
+                    lastTabStop.focus();
+                }
+
+                // TAB
+            } else {
+                if (document.activeElement === lastTabStop) {
+                    e.preventDefault();
+                    firstTabStop.focus();
+                }
+            }
         }
 
-      // TAB
-      } else {
-        if (document.activeElement === lastTabStop) {
-          e.preventDefault();
-          firstTabStop.focus();
+        // ESCAPE
+        if (e.keyCode === 27) {
+            closeModal();
         }
-      }
     }
 
-    // ESCAPE
-    if (e.keyCode === 27) {
-      closeModal();
-    }
-  }
-
-  // FIXME: hide non-modal content from screen readers
+    // hide wrapper content from screen reading when dialog box open 
+    document.querySelector('.wrapper').setAttribute('aria-hidden', 'true');
 }
 
 function closeModal() {
-  // Hide the modal and overlay
-  modal.style.display = 'none';
-  modalOverlay.style.display = 'none';
+    // Hide the modal and overlay
+    modal.style.display = 'none';
+    modalOverlay.style.display = 'none';
 
-  // Set focus back to element that had it before the modal was opened
-  focusedElementBeforeModal.focus();
+    // Set focus back to element that had it before the modal was opened
+    focusedElementBeforeModal.focus();
 
-  // FIXME: don't forget to make main content screen reader accessible again.
+    // unhide after dialog closed
+    document.querySelector('.wrapper').setAttribute('aria-hidden', 'false');
+
 }
